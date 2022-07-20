53bc80b702d99814dcbc805ab300205d6a7ae7fb
emgomes3112 <emgomes@outlook.com.br>
2022-07-20 13:51:43 -0300

    teste

---
+++ src/main/java/com/example/SisCad/SisCadApplication.java
@@ -0,0 +1,13 @@
+package com.example.SisCad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisCadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisCadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SisCad/SisCadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SisCad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisCadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
